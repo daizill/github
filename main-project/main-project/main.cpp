ce89f5bd66902edce050cfafc2e1bfb59541ecc6
Maxim.Lisov <lisov2023@inbox.ru>
2024-03-01 02:23:02 +0300

    Конфликт разрешен

--- main-project/main-project/main.cpp
+++ main-project/main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #7. Banking operations\n";
     cout << "Author: Lisov Maxim\n";
+    cout << "Group: 23PIng_1d\n";
     return 0;
 }
