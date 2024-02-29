d55d083d812dea179ccf63f485e003d2b02e586f
Maxim.Lisov <lisov2023@inbox.ru>
2024-03-01 02:01:17 +0300

    Реализовано форматирование данных при выводе в консоль

--- main-project/main-project/main.cpp
+++ main-project/main-project/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include "bank_operations.h"
 #include "file_reader.h"
 #include "constants.h"
 
@@ -9,21 +11,25 @@ int main()
     setlocale(LC_ALL, "Russian");
     cout << "Лабораторная работа №8. GIT\n";
     cout << "Вариант №7. Банковские операции\n";
-    cout << "Автор: Лисов Максим\n\n";
+    cout << "Автор: Максим Лисов\n\n";
     bank_operation* operations[MAX_FILE_ROWS_COUNT];
     int size;
     try
     {
-        read("data.txt", operations, size);
+        read("operations.txt", operations, size);
+        cout << "***** Банковские операции *****\n\n";
         for (int i = 0; i < size; i++)
         {
-            cout << operations[i]->operation_date.day << '.';
-            cout << operations[i]->operation_date.month << '.';
-            cout << operations[i]->operation_date.year << ' ';
-            cout << operations[i]->operation_hour << ':';
-            cout << operations[i]->operation_minute << ' ';
-            cout << operations[i]->account_type << ' ';
-            cout << operations[i]->amount << '\n';
+            /********** вывод информации о банковской операции **********/
+            cout << "Дата: " << setw(2) << setfill('0') << operations[i]->date.day << '.'
+                << setw(2) << setfill('0') << operations[i]->date.month << '.'
+                << setw(4) << setfill('0') << operations[i]->date.year << '\n';
+            cout << "Время: " << setw(2) << setfill('0') << operations[i]->time.hours << ':'
+                << setw(2) << setfill('0') << operations[i]->time.minutes << ':'
+                << setw(2) << setfill('0') << operations[i]->time.seconds << '\n';
+            cout << "Тип счета: " << operations[i]->account_type << '\n';
+            cout << "Сумма: " << operations[i]->amount << '\n';
+            cout << '\n';
         }
         for (int i = 0; i < size; i++)
         {
